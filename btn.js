750b8b7e0f3f1429824d34334d2ca90068277d0d
iamkaikai <iamkaikai@livemail.tw>
2023-07-05 14:31:42 -0400

    optimization

--- btn.js
+++ btn.js
@@ -1,39 +1,43 @@
 let intervalId;
 let intervalTime = 10;
 let min = Number.POSITIVE_INFINITY;
+let items = document.querySelectorAll('.css-rp1nxq');
+let btn = document.querySelector('.css-zqm9nz');
+let count = 0
 
 const intervalFunc = () => {
-    let items = document.querySelectorAll('.css-rp1nxq');
-    let btn = document.querySelector('.css-zqm9nz');
+    
     let textContents = Array.from(items).map(item => item.innerText);
     let time = parseInt(textContents.join(''));
-    let login = localStorage.getItem('login');
+    
     
     if (time < min) min = time;
 
     if (!isNaN(time)) {
         console.log(time);
         console.log("lowest time = " + min);
-        console.log("login = " + login);
     }
 
-    if (time >= 3000) {
-        intervalTime = 5000;
+    if (time >= 4000) {
+        intervalTime = 3000;
     } else if (time >= 1000) {
         intervalTime = 1000;
+    }else if (time >= 500) {
+        intervalTime = 100;
     } else {
         intervalTime = 10;
     }
 
     if (time <= 100) {
-        // Add some random delay up to 50 milliseconds before clicking the button
-        // let delay = Math.random() * 50;
         // setTimeout(() => {
         //     btn.click();
         //     console.log('Button clicked');
-        // }, delay);
-        btn.click();
-        console.log('Button clicked');
+        // }, Math.random() * 50);
+        if (count < 2){
+            btn.click();
+            console.log('Button clicked');
+            count +=1;    
+        }
     }
 
     resetTimer(intervalTime);
